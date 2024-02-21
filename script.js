5c037cc096db06f8fb5f38bb3fb60d3c3838c9d5
Vikkraman1120 <vikkraman09@gmail.com>
2024-02-21 18:49:31 +0530

    Login & signUp

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// function validation(){
+//     let userName = document.getElementById("username").value;
+//     let passWord = document.getElementById("password").value;
+
+//     if(userName == "vikram" && passWord == "1234"){
+//         window.location.href="sampleindex.html";
+//     }
+//     else{
+//         alert("pls try again");
+//     }
+// }
+
+
+function redirectToValidationPage() {
+    // Change the URL to the desired destination
+    window.location.href = "loginpage.html";
+}
