fc15a560643ab7aceb80c416f27e12b94d14bf5c
Vikkraman1120 <vikkraman09@gmail.com>
2024-02-27 19:24:27 +0530

    Navigation bar update

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-
-// function validation(){
-//     let userName = document.getElementById("username").value;
-//     let passWord = document.getElementById("password").value;
-
-//     if(userName == "vikram" && passWord == "1234"){
-//         window.location.href="sampleindex.html";
-//     }
-//     else{
-//         alert("pls try again");
-//     }
-// }
-
-
 function redirectToValidationPage() {
-    // Change the URL to the desired destination
     window.location.href = "loginpage.html";
 }
+
+function redirectToHomePage() {
+    window.location.href = "index.html";
+}
+
+
